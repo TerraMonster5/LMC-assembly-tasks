9c16255e62f46bce91591ac69a37e2fc7f952d61
Will <93338246+TerraMonster5@users.noreply.github.com>
2024-03-11 15:23:39 +0000

    Added labels

--- src/intermediate/task4.asm
+++ src/intermediate/task4.asm
@@ -6,16 +6,16 @@ INP
 STA num3
 LDA num1
 SUB num2
-BRP 15
+BRP compare
 LDA num1
 STA temp
 LDA num2
 STA num1
 LDA temp
 STA num2
-LDA num2
+compare LDA num2
 SUB num3
-BRP 36
+BRP output
 LDA num1
 SUB num3
 BRP 30
@@ -27,14 +27,14 @@ LDA num1
 STA num2
 LDA temp
 STA num1
-BRA 36
-LDA num2
+BRA ouput
+swap LDA num2
 STA temp
 LDA num3
 STA num2
 LDA temp
 STA num3
-LDA num1
+output LDA num1
 OUT
 LDA num2
 OUT
